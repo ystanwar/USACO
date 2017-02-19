acb5ba9373dfe4d5d3baa423a956237f0ceeff8c
ystanwar <ystanwar512@gmail.com>
2017-02-19 17:49:27 +0530

    Final solution ADHOC - greedy gift givers

--- gift.cpp
+++ gift.cpp
@@ -15,6 +15,7 @@ struct friends{
  string name;
  int id=0;
  int m=0;
+ int p;
 
 }v[10];
 
@@ -23,21 +24,23 @@ int getid(string s,int n)
 
     for(int i=0;i<n;i++)
     {
-        if(s==v[i].name);
-           {
-               cout<<v[i].name<<endl;
-                return i;
-           }
+
+              string s1=v[i].name;
+              if(s==s1)
+              {
+                  return v[i].id;
+              }
+
     }
-    return 0;
+    return -1;
 
 }
 int display(int n)
 {
  for(int i=0;i<n;i++)
     {
-
-               cout<<v[i].name<<" "<<v[i].m<<endl;
+                int ans=v[i].m-v[i].p;
+               cout<<v[i].name<<" "<<ans<<" "<<v[i].p<<endl;
 
     }
 }
@@ -54,38 +57,41 @@ int main() {
 
          fin>>v[i].name;
          v[i].id=i+1;
-         fout<<v[i].id<<"-->"<<v[i].name<<" ";
+        // fout<<v[i].id<<"-->"<<v[i].name<<" ";
 	}
 
 
-display(n);
 string s1;
 int m,nof;
 for(int i=0;i<n;i++)
 {
      fin>>s1;
-     cin>>m>>nof;
+     fin>>m>>nof;
      int don=getid(s1,n)-1;
-     v[don].m=m;
-     int mon=(m-m%nof)/nof;
-     if(nof==0||m==0)
+
+     v[don].m=v[don].m+m;
+     v[don].p=m;
+     if(nof==0)
      {
 
      }
       else{
+        int mon=(m-m%nof)/nof;
+
         v[don].m=v[don].m-(mon*nof);
+       cout<<v[don].name<<"-"<<v[don].m<<"<-->"<<v[don].p<<"--------";
         for(int j=0;j<nof;j++)
         {
             string fri;
             fin>>fri;
             int pos=getid(fri,n)-1;
             v[pos].m=v[pos].m+mon;
-
+          //  cout<<v[pos].name<<"-"<<v[pos].m<<"<-->";
         }
-
+cout<<endl;
       }
 }
-
+display(n);
 
     return 0;
 }
