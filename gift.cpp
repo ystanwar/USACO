de94c2a65175553e20b60fb480d156582c784bfb
ystanwar <ystanwar512@gmail.com>
2017-02-19 16:46:44 +0530

    ADHOC - greedy gift givers

--- gift.cpp
+++ gift.cpp
@@ -1,3 +1,4 @@
+
 /*
 ID: ystanwar
 PROG: gift
@@ -7,35 +8,82 @@ LANG: C++
 #include <fstream>
 #include <string>
 #include <vector>
+#include <string.h>
 using namespace std;
 
+struct friends{
+ string name;
+ int id=0;
+ int m=0;
+
+}v[10];
+
+int getid(string s,int n)
+{
+
+    for(int i=0;i<n;i++)
+    {
+        if(s==v[i].name);
+           {
+               cout<<v[i].name<<endl;
+                return i;
+           }
+    }
+    return 0;
+
+}
+int display(int n)
+{
+ for(int i=0;i<n;i++)
+    {
+
+               cout<<v[i].name<<" "<<v[i].m<<endl;
+
+    }
+}
 int main() {
     ofstream fout ("gift.out");
     ifstream fin ("gift.in");
 
 	int n;
 	fin>>n;
-	vector<string> v;
 	string str;
-	for(int i=0;i<n+1;i++)
-	{
-		getline(fin,str);
-		v.push_back(str);
-        //fout<<i<<"-->"<<v[i]<<" ";
-	}
-int net[n][1];
-vector<string> o;
-for(int i=0;i<=n;i++)
-{
-    if(i==0)
-    {
-        o.push_back("ok");
-    }
-    else{
-        getline(fin,str);
-        o.push_back(str);
 
-    }
+	for(int i=0;i<n;i++)
+	{
+
+         fin>>v[i].name;
+         v[i].id=i+1;
+         fout<<v[i].id<<"-->"<<v[i].name<<" ";
+	}
+
+
+display(n);
+string s1;
+int m,nof;
+for(int i=0;i<n;i++)
+{
+     fin>>s1;
+     cin>>m>>nof;
+     int don=getid(s1,n)-1;
+     v[don].m=m;
+     int mon=(m-m%nof)/nof;
+     if(nof==0||m==0)
+     {
+
+     }
+      else{
+        v[don].m=v[don].m-(mon*nof);
+        for(int j=0;j<nof;j++)
+        {
+            string fri;
+            fin>>fri;
+            int pos=getid(fri,n)-1;
+            v[pos].m=v[pos].m+mon;
+
+        }
+
+      }
 }
 
 
